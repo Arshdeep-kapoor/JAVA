7d644537a9531ae165b06b0e42405ae0f2776d42
Arshdeep-kapoor <arshdeep.d.gr8.1@gmail.com>
2017-04-29 15:57:49 -0400

    Add files via upload

---
+++ chapter7/ques03.java
@@ -0,0 +1,31 @@
+package chapter07;
+import java.util.Scanner;
+/**
+ * @author Arshdeep Kapoor
+ * 
+ */
+public class ques03 {
+	public static void main(String[] args) {
+		int[] counts = new int[100]; 
+
+		System.out.print("Enter the integers between 1 and 100: ");
+
+		count(counts);
+
+		for (int i = 0; i < counts.length; i++) {
+			if (counts[i] > 0)
+				System.out.println((i + 1) + " occurs " + counts[i] +
+					" time" + (counts[i] > 1 ? "s" : ""));
+		}
+	}
+
+	public static void count(int[] counts){
+		Scanner in = new Scanner(System.in);
+		int num; 
+		do {
+			num = in.nextInt();
+			if (num >= 1 && num <= 100)	
+				counts[num - 1]++;
+		} while (num != 0);
+	}
+}
