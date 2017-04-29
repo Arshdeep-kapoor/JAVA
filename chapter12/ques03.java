18341a4a19bb77593976c22827ef5cad70a3a216
Arshdeep-kapoor <arshdeep.d.gr8.1@gmail.com>
2017-04-29 16:01:24 -0400

    Add files via upload

---
+++ chapter12/ques03.java
@@ -0,0 +1,30 @@
+package chapter12;
+import java.util.*;
+/**
+ * @author Arshdeep Kapoor
+ * 
+ */
+public class ques03 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		int[] array = getArray();
+
+		System.out.print("Enter the index of the array: ");
+		try {
+			System.out.println("The corresponding element value is " + 
+				array[in.nextInt()]);
+		}
+		catch (ArrayIndexOutOfBoundsException ex) {
+			System.out.println("Out of Bounds.");
+		}
+	}
+
+	public static int[] getArray() {
+		int[] array = new int[100];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int)(Math.random() * 100) + 1;
+		}
+		return array;
+	}
+}
