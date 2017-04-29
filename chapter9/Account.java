82b190dde5c17f6b81af8329177b01cb6e315fd2
Arshdeep-kapoor <arshdeep.d.gr8.1@gmail.com>
2017-04-29 15:59:21 -0400

    Add files via upload

---
+++ chapter9/Account.java
@@ -0,0 +1,69 @@
+package chapter09;
+import java.util.Date;
+/**
+ * @author Arshdeep Kapoor
+ * 
+ */
+public class Account {
+	private int id;
+	private double balance;
+	private static double annualInterestRate;
+	private Date dateCreated;
+
+	Account() {
+		id = 0;
+		balance = 0;
+		annualInterestRate = 0;
+		dateCreated = new Date(); 
+	}
+
+	Account(int newId, double newBalance) {
+		id = newId;
+		balance = newBalance;
+		dateCreated = new Date();
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int newId) {
+		id = newId;
+	}
+
+	public void setBalance(double newBalance) {
+		balance = newBalance;
+	}
+
+	public void setAnnualInterestRate(double newAnnualInterestRate) {
+		annualInterestRate = newAnnualInterestRate;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public double getAnnualInterestRate() {
+		return annualInterestRate;
+	}
+
+	public String getDateCreated() {
+		return dateCreated.toString();
+	}
+
+	public double getMonthlyInterestRate() {
+		return annualInterestRate / 12;
+	}
+
+	public double getMonthlyInterest() {
+		return balance * (getMonthlyInterestRate() / 100);
+	}
+
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+
+	public void deposit(double amount) {
+		balance += amount;
+	}
+}
---
+++ chapter9/ques07.java
@@ -0,0 +1,25 @@
+package chapter09;
+/**
+ * @author Arshdeep Kapoor
+ * 
+ */
+public class ques07 {
+	public static void main(String[] args) {
+		
+		Account account = new Account(1122, 20000);
+
+		account.setAnnualInterestRate(4.5);
+
+		account.withdraw(2500);
+
+		account.deposit(3000);
+
+		System.out.println("\n          Account Statement");
+		System.out.println("------------------------------------------");
+		System.out.println("Account ID: " + account.getId());
+		System.out.println("Date created: " + account.getDateCreated());
+		System.out.printf("Balance: $%.2f\n", account.getBalance());
+		System.out.printf("Monthly interest: $%.2f\n", 
+			account.getMonthlyInterest());
+	}
+}
